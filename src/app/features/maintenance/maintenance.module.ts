33a836777d6daa31213c4c70078153d082281be7
Aliaksei_Lasevich <alexey.losevich@gmail.com>
2023-10-16 16:44:45 +0200

    Refactoring maintenance module

--- src/app/features/maintenance/maintenance.module.ts
+++ src/app/features/maintenance/maintenance.module.ts
@@ -10,19 +10,26 @@ import {BuildingsComponent} from './buildings/buildings.component';
 import {ConfirmationDialogComponent} from './confirmation-dialog/confirmation-dialog.component';
 import {MaterialModule} from "../../shared/material/material.module";
 import {ReactiveFormsModule} from "@angular/forms";
+import {MaintenanceComponent} from './maintenance/maintenance.component';
+import {HeaderModule} from "../header/header.module";
 
-const routes: Routes = [{path: 'buildings', component: BuildingsComponent}];
+const routes: Routes = [{
+  path: 'maintenance', component: MaintenanceComponent, children: [
+    {path: 'buildings', component: BuildingsComponent}
+  ]
+}]
 
 @NgModule({
   declarations: [
     BuildingsComponent,
     BuildingFormComponent,
     ConfirmationDialogComponent,
+    MaintenanceComponent,
   ],
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule.forChild(routes),
+    RouterModule.forChild(routes), HeaderModule,
     // EffectsModule.forFeature(PlannerStoreEffects),
     MaterialModule,
     ReactiveFormsModule
